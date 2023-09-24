57e9307ff4d6e7e978dcea543000a2d6bcee9e50
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-09-24 12:09:36 +0530

    app.post(/)

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
-from fastapi import Request
+from fastapi import Request, HTTPException
 from fastapi.responses import JSONResponse
+import json
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.post("/")
 async def handle_request(request: Request):
-    payload = await request.json()
-    intent = payload['queryResult']['intent']['displayName']
-    parameters = payload['queryResult']['parameters']
-    outputs = payload['queryResult']['outputContexts']
-    if intent == 'track.order - context: ongoing-tracking':
-        return JSONResponse(content={
-            "fulfillmentText": f"Received =={intent}== in the backend"
-        })
+    try:
+        payload = await request.json()
+        intent = payload['queryResult']['intent']['displayName']
+        parameters = payload['queryResult']['parameters']
+        outputs = payload['queryResult']['outputContexts']
+        if intent == 'track.order - context: ongoing-tracking':
+            return JSONResponse(content={
+                "fulfillmentText": f"Received =={intent}== in the backend"
+            })
+        else:
+            return JSONResponse(content={"fulfillmentText": "Intent not recognized"})
+    except json.JSONDecodeError as e:
+        raise HTTPException(status_code=400, detail="Invalid JSON data")

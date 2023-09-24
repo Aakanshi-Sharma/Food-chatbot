05f0935ab31172d3d1aa0e877190d102001cd988
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-09-24 10:42:11 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def handle_request(request: Request):
+    payload = await request.json()
+    intent = payload['queryResult']['intent']['displayName']
+    parameters = payload['queryResult']['parameters']
+    outputs = payload['queryResult']['outputContexts']
+    if intent == 'track.order - context: ongoing-tracking':
+        return JSONResponse(content={
+            "fulfillmentText": f"Received =={intent}== in the backend"
+        })

929609486ee69de5b10765fb892de41bd01e33c7
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-10-17 17:22:55 +0530

    changes

--- chat/src/components/Home/index.js
+++ chat/src/components/Home/index.js
@@ -5,8 +5,9 @@ const Home = () => {
     <div className="outerdiv">
       <div className="innerdiv">
         <iframe
+          className="chatBot"
           allow="microphone;"
-          width="350"
+          width="400"
           height="500"
           src="https://console.dialogflow.com/api-client/demo/embedded/fada8c31-74f8-42c4-8b48-c8decfa744bb"
         ></iframe>
